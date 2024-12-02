7dde59ad20cc93ac01b38d18e23d0302cf1368f3
SeoJaeWan <sjw7324@naver.com>
2024-12-02 16:07:05 +0900

    feat: docker

---
+++ next-app/src/components/organisms/room/gameBoard/gameBoard.style.ts
@@ -0,0 +1,91 @@
+import CardStyle from "@/components/atoms/room/card/card.style";
+import { timePeriod } from "@/hooks/useGame";
+import toRem from "@/styles/utils/toRem";
+import styled from "styled-components";
+
+interface IContainer {
+  $timePeriod: timePeriod;
+}
+
+const Text = styled.p`
+  margin-bottom: 20px;
+
+  font-size: ${toRem(20)};
+  font-weight: 800;
+`;
+
+const CardList = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 10px;
+`;
+
+interface IPlayBoard {
+  $isAnimationFinish: boolean;
+}
+
+const PlayBoard = styled.div<IPlayBoard>`
+  display: ${(props) => (props.$isAnimationFinish ? "block" : "none")};
+
+  width: 100%;
+  height: 100%;
+`;
+
+const Container = styled.div<IContainer>`
+  position: relative;
+
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+
+  width: 100%;
+  height: 100%;
+
+  padding: 20px;
+
+  overflow-y: auto;
+
+  background-color: ${(props) =>
+    props.$timePeriod === "night"
+      ? "var(--day-background-night)"
+      : "var(--day-background-morning)"};
+
+  ${Text} {
+    color: ${(props) =>
+      props.$timePeriod === "night"
+        ? "var(--day-background-morning)"
+        : "var(--day-background-night)"};
+  }
+`;
+
+const Selector = styled.button`
+  background-color: transparent;
+  border: none;
+
+  width: 150px;
+  height: auto;
+  aspect-ratio: 63/88;
+
+  @media (max-width: 768px) {
+    width: calc((100% - 20px) / 2);
+  }
+
+  ${CardStyle.Container} {
+    width: 100%;
+    height: 100%;
+
+    @media (max-width: 768px) {
+      width: 100%;
+    }
+  }
+`;
+
+const GameBoardStyle = {
+  Container,
+  Text,
+  CardList,
+  PlayBoard,
+  Selector,
+};
+
+export default GameBoardStyle;
