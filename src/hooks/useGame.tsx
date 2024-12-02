ee9767b27e38718f0a61cc460b052fd0476c2758
SeoJaeWan <sjw7324@naver.com>
2024-12-02 19:24:55 +0900

    feat: docker compose

--- src/hooks/useGame.tsx
+++ src/hooks/useGame.tsx
@@ -121,7 +121,7 @@ export type Turn =
   | "citizenWin" // 시민 승리
   | "gameFinish"; // 게임 종료
 
-const socketUrl = process.env.NEXT_PUBLIC_WEBSOCKET_SERVER;
+const socketUrl = "/server";
 
 const defaultPlayerRoles = Object.values(playableRoles).reduce(
   (acc, cur) => ({
