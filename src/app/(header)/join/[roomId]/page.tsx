97bad15e4f01846596df8486ac260cfbfb4d3bf6
SeoJaeWan <sjw7324@naver.com>
2024-12-10 10:08:42 +0900

    feat: seo

--- src/app/(header)/join/[roomId]/page.tsx
+++ src/app/(header)/join/[roomId]/page.tsx
@@ -2,7 +2,21 @@
 
 import EnterTemplate from "@/components/templates/enter";
 import useGame from "@/hooks/useGame";
+import { Metadata } from "next";
 import { useParams } from "next/navigation";
+
+export const metadata: Metadata = {
+  title: "마피아 게임 - 방 참가",
+  description: "마피아 게임 초대가 왔어요.",
+  keywords: "마피아, 게임, 마피아게임, 경찰, 의사, 시민, 마피아",
+
+  openGraph: {
+    title: "마피아 게임",
+    description: "마피아 게임 초대가 왔어요.",
+    type: "website",
+    locale: "ko_KR",
+  },
+};
 
 const Join = () => {
   const { roomId } = useParams<{ roomId: string }>();
